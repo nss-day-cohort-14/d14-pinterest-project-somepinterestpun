1e63c8bc42c509fb50414703959e6e11b4f780fd
CJT1105 <CJT1105@GMAIL.COM>
2016-07-25 12:02:08 -0500

    added navbar

---
+++ app/controllers/NavCtrl.js
@@ -0,0 +1,22 @@
+app.controller('NavCtrl', function($scope){
+	$scope.login = function(){
+		let provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().signInWithPopup(provider);
+	}
+	$scope.logout = function(){
+		firebase.auth().signOut();
+		console.log("signed out" );
+	}
+	firebase.auth().onAuthStateChanged(function(user){
+		if(user){
+			$scope.$apply(function(){
+				$scope.loggedin = true;
+			})
+		}
+		else {
+			$scope.$apply(function(){
+				$scope.loggedin = false;
+			})
+		}
+	})
+})
