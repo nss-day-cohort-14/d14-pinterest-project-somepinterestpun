b63143db45e3d8b15883337da1b1f538d36694c0
CJT1105 <CJT1105@GMAIL.COM>
2016-07-25 14:02:43 -0500

    added login logic

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,8 +1,27 @@
 "use strict";
-app.controller('NavCtrl', function($scope, $location){
+app.controller('NavCtrl', function($scope, $location, UserFactory){
 	$scope.login = function(){
 		let provider = new firebase.auth.GoogleAuthProvider();
-		firebase.auth().signInWithPopup(provider);
+		firebase.auth().signInWithPopup(provider).then(function(user){
+			UserFactory.getUserList()
+			.then(function(userList){
+				let currentUser = firebase.auth().currentUser;
+				let currentUid = firebase.auth().currentUser.uid;
+				let userArray = [];
+				for (user in userList){
+					let index = userList[user];
+					if(currentUid != index.uid){
+						let userObject = {
+							name: currentUser.displayName,
+							email: currentUser.email,
+							uid: currentUser.uid
+						}
+						userArray.push(userObject);
+					}
+				}
+				UserFactory.createUser(userArray[0])
+			})
+		})
 	}
 	$scope.logout = function(){
 		firebase.auth().signOut();
