6df45766367c4c31ec0d9e97a2a4d77926c96fe0
iamtylerd <tylerdanielphotography@gmail.com>
2016-07-25 12:54:51 -0500

    merged app and nav

--- app/app.js
+++ app/app.js
@@ -1,1 +1,31 @@
-var app = angular.module('app', ['ngRoute']);
+"use strict";
+
+var app = angular.module('app', ['ngRoute'])
+.constant('FirebaseURL', "https://group-project-somepinterestpun.firebaseio.com/");
+
+app.config(function($routeProvider) {
+    // let authConfig = {
+    //     apiKey: FBCreds.apiKey,
+    //     authDomain: FBCreds.authDomain
+    // };
+    // firebase.initializeApp(authConfig);
+    
+    $routeProvider.
+        when('/', {
+        	templateUrl: 'splash.html',
+        	controller: 'NavCtrl'
+        }).
+        when('/profile', {
+            templateUrl: 'partials/profile.html',
+            controller: 'ProfileCtrl'
+        }).
+        when('/profile/board', {
+            templateUrl: 'partials/board.html',
+            controller: 'BoardCtrl'
+        }).
+        when('/profile/board/:pinId', {
+            templateUrl: 'partials/pin.html',
+            controller: 'PinCtrl'
+        }).
+        otherwise('/profile');
+});
--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -1,11 +1,13 @@
-app.controller('NavCtrl', function($scope){
+"use strict";
+app.controller('NavCtrl', function($scope, $location){
 	$scope.login = function(){
 		let provider = new firebase.auth.GoogleAuthProvider();
 		firebase.auth().signInWithPopup(provider);
 	}
 	$scope.logout = function(){
 		firebase.auth().signOut();
-		console.log("signed out" );
+		$location.url("/");
+		console.log("signed out");
 	}
 	firebase.auth().onAuthStateChanged(function(user){
 		if(user){
