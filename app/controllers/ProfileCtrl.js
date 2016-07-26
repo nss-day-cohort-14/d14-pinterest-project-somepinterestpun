573a3e02ec5882d5d72831f23360b36743141f26
Mike Meadows <mmeadow33@gmail.com>
2016-07-26 14:11:38 -0500

    forgot this file

--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -1,21 +1,21 @@
 "use strict";
 
-app.controller('ProfileCtrl', function($scope, $location, BoardFactory, UserFactory, FirebaseURL) {
+app.controller('ProfileCtrl', function($scope, $location, BoardFactory, UserFactory, FirebaseURL, localStorageService) {
 	BoardFactory.getBoards()
 	.then(function(boardCollection) {
 		$scope.boards = boardCollection
 		return boardCollection;
 	})
-	
 
-	$scope.DisplayUserDetails = function () {
-		let userObj = {}
-		 userObj.name = firebase.auth().currentUser.displayName;
-		 userObj.email = firebase.auth().currentUser.email;
-		return userObj
-	}
-	$scope.user = $scope.DisplayUserDetails()
-	console.log($scope.user);
+	//
+	// $scope.DisplayUserDetails = function () {
+	// 	let userObj = {}
+	// 	 userObj.name = firebase.auth().currentUser.displayName;
+	// 	 userObj.email = firebase.auth().currentUser.email;
+	// 	return userObj
+	// }
+	// $scope.user = $scope.DisplayUserDetails()
+	// console.log($scope.user);
 
 	$scope.Remove = function (removeId) {
 		BoardFactory.deleteBoard(removeId)
@@ -26,11 +26,11 @@ app.controller('ProfileCtrl', function($scope, $location, BoardFactory, UserFact
 			});
 		});
 	}
-
+let currentUser = localStorageService.get("currentUser");
 	$scope.newBoard = {
 		description: "",
 		title: "",
-		uid: firebase.auth().currentUser.uid
+		uid: currentUser.uid
 	};
 
 	$scope.AddBoard = function () {
